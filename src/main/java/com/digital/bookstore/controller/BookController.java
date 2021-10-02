f1c890b4b261d8a9e8aa9218acc71d65891bcb8d
kanchanvit <kanchan.vit@gmail.com>
2021-10-02 18:47:44 +0530

    commited

--- src/main/java/com/digital/bookstore/controller/BookController.java
+++ src/main/java/com/digital/bookstore/controller/BookController.java
@@ -2,6 +2,7 @@ package com.digital.bookstore.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -40,7 +41,7 @@ public class BookController {
 		return ResponseEntity.of(this.bookService.update(bookRequest));
 	}
 	
-	@PutMapping
+	@DeleteMapping
 	public ResponseEntity<Status> delete(@RequestParam Integer id){
 		//return ResponseEntity.of(this.bookService.update(BookRequest));
 		return ResponseEntity.of(this.bookService.delete(id));
